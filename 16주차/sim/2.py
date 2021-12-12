9babcdb1a5dc03d0f84657993228c6e48872a49b
Macsim2 <tmvhswlqkq7@naver.com>
2021-12-12 23:54:13 +0900

    난 딴돈의 반만 가져가

---
+++ 16주차/sim/2.py
@@ -0,0 +1,17 @@
+# 거스름 돈
+def solution(n, money):
+    dp = [[0]*(n+1) for _ in range(len(money))]
+
+    dp[0][0] = 1
+
+    for i in range(money[0], n+1, money[0]):
+        dp[0][i] = 1
+
+    for y in range(1, len(money)):
+        for x in range(n+1):
+            if x >= money[y]:
+                dp[y][x] = (dp[y-1][x] + dp[y][x - money[y]]) % 1000000007
+            else:
+                dp[y][x] = dp[y-1][x]
+
+    return dp[-1][-1]
